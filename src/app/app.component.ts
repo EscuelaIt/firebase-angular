9a46c3a5e965c3ca22fcb9c611e80f419e5de250
jorgeucano <jorgeucano@gmail.com>
2017-02-07 11:28:26 -0300

    Angularfire2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import { AuthProviders, AuthMethods } from 'angularfire2';
+
+import {Subject } from 'rxjs/Subject';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,84 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  items: any;
+  usuariosLogueados:FirebaseListObservable<any[]>;
+
+  subject:Subject<any>;
+
+  queryObservable:any;
+
+ 
+  constructor(private af: AngularFire){
+    this.usuariosLogueados = 
+            this.af.database.list('/usuarios');
+    //const promiseObject = this.af.database.object('/usuarios');
+    //promiseObject.set({name: "jorge"});
+
+    this.subject = new Subject();
+    
+    this.af.database.list('/usuarios',{
+                              query:{
+                                orderByChild: 'size',
+                                equalsTo: this.subject
+                              }
+                          });
+
+    this.queryObservable.subscribe(queriedItems=>{
+        console.log(queriedItems);
+    });
+     
+  }
+  
+
+  filterBy(valor: string) {
+    this.subject.next(valor); 
+  }
+
+  login(){
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    }).then((authUser)=>{
+      console.log(authUser);
+      this.usuariosLogueados.push({ 
+        "nombre": authUser.auth.displayName,
+        "email": authUser.auth.email,
+        "photoUrl": authUser.auth.photoURL
+      });
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
+
+  ngOnInit(){
+    this.items = this.af.database.list("/chats");
+
+    
+  }
+
+  eliminar(key:string){
+    this.usuariosLogueados.remove(key);
+  }
+
+  update(key:string){
+    this.usuariosLogueados.update(key, {'email': 'jorge@jorge.com'});
+  }
+
+  eliminarTodos(){
+    this.af.database.object('/usuarios').remove();
+  }
+
+  snapshot(){
+    this.items = this.af.database.object('/usuarios', {preserveSnapshot: true});
+    this.items.subscribe(snapshot=>{
+      console.log(snapshot.key);
+      console.log(snapshot.val());
+    });
+  }
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,23 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+
+import { AngularFireModule } from 'angularfire2';
+import { AuthProviders, AuthMethods } from 'angularfire2';
+export const FIREBASECONFIG = {
+  apiKey: "AIzaSyBfbCSWlceOXo1NKvOC6TFATmDunOlkZeU",
+  authDomain: "miwhatit.firebaseapp.com",
+  databaseURL: "https://miwhatit.firebaseio.com",
+  storageBucket: "miwhatit.appspot.com",
+  messagingSenderId: "1048077916191"
+};
+
+export const FIREBASEAUTHCONFIG ={
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +29,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(FIREBASECONFIG, FIREBASEAUTHCONFIG)
   ],
   providers: [],
   bootstrap: [AppComponent]
